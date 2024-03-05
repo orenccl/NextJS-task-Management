8ad45805a13699a11608f4ff8015f69ade15b373
Jun <oren.ccl@gmail.com>
2024-03-05 10:50:19 +0800

    [+] Add Unit Test

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,63 @@
+import { Test } from '@nestjs/testing';
+import { TasksService } from './tasks.service';
+import { TasksRepository } from './tasks.repository';
+import { NotFoundException } from '@nestjs/common';
+
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+  findOne: jest.fn(),
+});
+
+const mockUser = {
+  id: 'someId',
+  username: 'someUsername',
+  password: 'somePassword',
+  tasks: [],
+};
+
+describe('TasksService', () => {
+  let tasksService: TasksService;
+  let tasksRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TasksRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    tasksService = await module.get<TasksService>(TasksService);
+    tasksRepository = await module.get<TasksRepository>(TasksRepository);
+  });
+
+  describe('getTasks', () => {
+    it('calls TaskRepository.getTasks and returns the result', async () => {
+      tasksRepository.getTasks.mockResolvedValue('someValue');
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(result).toEqual('someValue');
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('calls TaskRepository.findOne and returns the result', async () => {
+      const mockTask = {
+        title: 'Test task',
+        description: 'Test desc',
+        id: 'someId',
+        status: 'OPEN',
+      };
+
+      tasksRepository.findOne.mockResolvedValue(mockTask);
+      const result = await tasksService.getTaskById('someId', mockUser);
+      expect(result).toEqual(mockTask);
+    });
+
+    it('calls TaskRepository.findOne and handles an error', async () => {
+      tasksRepository.findOne.mockResolvedValue(null);
+      expect(tasksService.getTaskById('someId', mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+});
